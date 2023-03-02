8d76deb418904d53fdce0fc17440b4071926862f
Bharath Kalyan S <sbharath35@gmail.com>
2023-03-02 16:12:55 +0530

    Done with SetUp and Configuration

---
+++ src/main/java/com/bharathkalyans/databasetoexcel/DataBaseToExcelApplication.java
@@ -0,0 +1,13 @@
+package com.bharathkalyans.databasetoexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseToExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBaseToExcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bharathkalyans/databasetoexcel/DataBaseToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharathkalyans.databasetoexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseToExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
