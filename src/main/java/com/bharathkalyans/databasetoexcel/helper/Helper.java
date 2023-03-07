f7efccb41e16c6f7621893d9769925a572d220e1
Bharath Kalyan S <sbharath35@gmail.com>
2023-03-07 11:12:14 +0530

    Added Helper Class to convert the Data from List to Excel Sheet!

---
+++ src/main/java/com/bharathkalyans/databasetoexcel/helper/Helper.java
@@ -0,0 +1,77 @@
+package com.bharathkalyans.databasetoexcel.helper;
+
+import com.bharathkalyans.databasetoexcel.model.UserDetails;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class Helper {
+
+    //user_id |email|   first_name   |   gender    |   last_name
+    public static String[] HEADERS = {"USER_ID", "EMAIL", "FIRST_NAME", "GENDER", "LAST_NAME"};
+
+    public static String SHEET_NAME = "user_details";
+
+    public static ByteArrayInputStream dataToExcel(List<UserDetails> userDetailsList) throws IOException {
+
+        //1.WorkBook
+        Workbook workBook = new XSSFWorkbook();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+
+            //2.Sheet
+            Sheet sheet = workBook.createSheet(SHEET_NAME);
+
+            //3.Access first Row
+            Row row = sheet.createRow(0);
+
+            //4.Build the Headers through Loop.
+            for (int i = 0; i < HEADERS.length; i++) {
+                Cell cell = row.createCell(i);
+                cell.setCellValue(HEADERS[i]);
+            }
+
+            //5.Traverse through the list and add the data!.
+            int rowIndex = 1;
+            for (UserDetails userDetails : userDetailsList) {
+                Row dataRow = sheet.createRow(rowIndex);
+                dataRow.createCell(0).setCellValue(userDetails.getUserId());
+                dataRow.createCell(1).setCellValue(userDetails.getEmail());
+                dataRow.createCell(2).setCellValue(userDetails.getFirstName());
+                dataRow.createCell(3).setCellValue(userDetails.getGender());
+                dataRow.createCell(4).setCellValue(userDetails.getLastName());
+                rowIndex++;
+            }
+
+            workBook.write(out);
+
+            return new ByteArrayInputStream(out.toByteArray());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Unable to generate Excel File!");
+        } finally {
+            workBook.close();
+            out.close();
+        }
+
+        return new ByteArrayInputStream(out.toByteArray());
+
+    }
+}
+
+
+
+
+
+
+
+
--- src/main/java/com/bharathkalyans/databasetoexcel/model/UserDetails.java
+++ src/main/java/com/bharathkalyans/databasetoexcel/model/UserDetails.java
@@ -28,6 +28,14 @@ public class UserDetails {
         this.gender = gender;
     }
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
     public String getFirstName() {
         return firstName;
     }
